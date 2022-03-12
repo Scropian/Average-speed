ad565a33fd8456070a2a9752c148421026a1c5ea
Zia ulislam <101190200+Scropian@users.noreply.github.com>
2022-03-12 09:13:40 +0500

    Add files via upload

---
+++ lab4.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.mavenproject2;
+import java.util.Scanner;
+public class lab4 {
+static Scanner console= new Scanner(System.in);
+public static void main(String [] args){
+    int d,s,i;
+    float t,avg;
+    s=(int)60;
+    i=(int)70;
+    d=(int)770;
+    t=(float)d/s;
+    System.out.println("Total time Taken is="+t);
+    avg=(float)d/i;
+    System.out.println("Total time Taken with increase in time is="+avg);
+    
+}    
+}
